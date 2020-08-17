f9ac613580c95a68bd6060ca5222d8860ed79e9c
Jesus DeLintt <jesus.delintt@gmail.com>
2020-08-17 10:42:57 -0700

    installed packages

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
-const fs = require("fs");
-const axios = require("axios");
+// const fs = require("fs");
+// const axios = require("axios");
 const inquirer = require("inquirer");
+
 inquirer.prompt({
   message: "Enter your GitHub username:",
   name: "username",
