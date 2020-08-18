97dfe867f622eb995b220227c5ae09e99354634d
Jesus DeLintt <jesus.delintt@gmail.com>
2020-08-17 17:03:10 -0700

    created another prompt for title and description

--- Develop/index.js
+++ Develop/index.js
@@ -1,23 +1,31 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-
-inquirer.prompt({
-  message: "Enter your GitHub username:",
-  name: "username",
-  message: "Provide a description of your project",
-  name: "description",
-  message: "How do I install this program?",
-  name: "install",
-});
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your ReadMe?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "What does your ReadMe do?",
+    },
+  ])
+  .then((answers) => {
+    console.info("Answer:", answers.title);
+    console.info(answers.description);
+  });
 // array of questions for user
 const questions = [];
 
-// function to write README file
+// // function to write README file
 function writeToFile(fileName, data) {}
 
-// function to initialize program
+// // function to initialize program
 function init() {}
 
-// function call to initialize program
+// // function call to initialize program
 init();
