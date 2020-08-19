923386e7461609b4ec73f4bb158c954ce3c6ef94
Jesus DeLintt <jesus.delintt@gmail.com>
2020-08-18 20:01:48 -0700

    put the questions in an array

--- Develop/index.js
+++ Develop/index.js
@@ -1,67 +1,53 @@
 const fs = require("fs");
-const axios = require("axios");
+
 const inquirer = require("inquirer");
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "title",
-      message: "What is the title of your ReadMe?",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "What does your ReadMe do?",
-    },
-    {
-      type: "checkbox",
-      name: "tableofcontents",
-      choices: ["Index", new inquirer.Separator(), "indexA"],
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: "How do you install this app?",
-    },
-    {
-      type: "list",
-      name: "license",
-      message: ["Choice1", "Choice2"],
-    },
-    {
-      type: "input",
-      name: "contributing",
-      message: "What is allowed on this site?",
-    },
-    {
-      type: "input",
-      name: "test",
-      message: "How do you run this code?",
-    },
-    {
-      type: "input",
-      name: "questions",
-      message: "Do you have any questions?",
-    },
-  ])
-  .then((answers) => {
-    console.info("Answer:", answers.title);
-    console.info(answers.description);
-  });
-// array of questions for user
-const questions = [];
+("use strict");
 
-// // function to write README file
-fs.readFile('README.md', data, err => {
+var questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your ReadMe?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What does your ReadMe do?",
+  },
+  {
+    type: "checkbox",
+    name: "tableofcontents",
+    choices: ["Index", new inquirer.Separator(), "indexA"],
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How do you install this app?",
+  },
 
-	
-	if (err) {
-		console.error(err)
-		return
+  {
+    type: "list",
+    name: "license",
+    message: ["Choice1", "Choice2"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "What is allowed on this site?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "How do you run this code?",
+  },
+  {
+    type: "input",
+    name: "questions",
+    message: "Do you have any questions?",
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  console.log("\nOrder receipt:");
+  console.log(JSON.stringify(answers, null, "  "));
 });
-
-// // function to initialize program
-function init() {}
-
-// // function call to initialize program
-init();
